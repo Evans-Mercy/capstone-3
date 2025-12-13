622acae317419f574b00f4ddca7fd6df14ca6fab
Evans-Mercy <cheria.mercy@gmail.com>
2025-12-12 19:11:06 -0600

    initial commit

--- backend-api/src/main/java/org/yearup/controllers/CategoriesController.java
+++ backend-api/src/main/java/org/yearup/controllers/CategoriesController.java
@@ -1,8 +1,7 @@
 package org.yearup.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 import org.yearup.data.CategoryDao;
 import org.yearup.data.ProductDao;
 import org.yearup.models.Category;
@@ -13,32 +12,38 @@ import java.util.List;
 // add the annotations to make this a REST controller
 // add the annotation to make this controller the endpoint for the following url
     // http://localhost:8080/categories
-// add annotation to allow cross site origin requests
+// add annotation to allow cross site origin requests - done
+@RestController
 public class CategoriesController
 {
-    private CategoryDao categoryDao;
+    private final CategoryDao categoryDao;
     private ProductDao productDao;
 
 
-    // create an Autowired controller to inject the categoryDao and ProductDao
-
-    // add the appropriate annotation for a get action
-    public List<Category> getAll()
-    {
-        // find and return all categories
-        return null;
+    // create an Autowired controller to inject the categoryDao and ProductDao - done
+    @Autowired
+    public CategoriesController(CategoryDao categoryDao) {
+        this.categoryDao = categoryDao;
     }
 
-    // add the appropriate annotation for a get action
-    public Category getById(@PathVariable int id)
-    {
-        // get the category by id
-        return null;
+    // add the appropriate annotation for a get action -done
+    @RequestMapping(path="/categories", method = RequestMethod.GET)
+    public List<Category> getAll(){
+        System.out.println("Here's all categories");
+        // find and return all categories - done
+        return this.categoryDao.getAllCategories();
+    }
+
+    // add the appropriate annotation for a get action - done
+    @RequestMapping(path="/categories/{identifier}", method=RequestMethod.GET)
+    public Category getById(@PathVariable int identifier) {
+        // get the category by id - done
+        return this.categoryDao.getById(identifier);
     }
 
     // the url to return all products in category 1 would look like this
     // https://localhost:8080/categories/1/products
-    @GetMapping("{categoryId}/products")
+    @GetMapping("/categories/{categoryId}/products")
     public List<Product> getProductsById(@PathVariable int categoryId)
     {
         // get a list of product by categoryId
